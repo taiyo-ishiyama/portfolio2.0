beae7e3c9d02552ee0359b79b79db234aa26db43
taiyo-ishiyama <taiyou04142001@gmail.com>
2026-02-15 21:24:21 +1000

    Fixed CI error

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests/e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
   use: {
-    baseURL: "http://localhost:3000"
-  }
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+  webServer: {
+    command: "pnpm start",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
 });
